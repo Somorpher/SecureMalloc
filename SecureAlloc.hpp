6585024e46827842f2fe48015c1550bf492ed45b
Somorpher <auc_04@proton.me>
2024-08-01 18:24:28 +0200

    Update SecureAlloc.hpp

--- SecureAlloc.hpp
+++ SecureAlloc.hpp
@@ -9,17 +9,36 @@
 #include <sstream>     // for std::stringstream
 #include <stdexcept>   // for std::stoll
 #include <type_traits> // for std::is_array_v, std::is_pointer_v, std::enable_if, and std::is_reference_v
-
-// Conditional includes to avoid multiple inclusions
-#ifndef _STDLIB_H
 #include <stdlib.h>
-#endif
-
-#ifndef _NEW_
 #include <new>
+
+#if defined(__GNUC__) || defined(__clang__)
+
+#define __a_inj_dc __attribute__((nothrow, leaf, optimize("3"), section(".text"))) // attribute injection for dc(default constructor)
+#define __a_inj_cctor __attribute__((nothrow, leaf, optimize("3"), access(read_only, 1))) // attribute injection for copy constructor
+#define __a_inj_mctor __attribute__((nothrow, leaf, optimize("3"), access(read_write, 1))) // attribute injection for move constructor
+#define __a_inj_eqop __attribute__((nothrow, leaf, optimize("3"), access(read_only, 1))) // attribute injection for equality operator
+#define __a_inj_uneqop __attribute__((nothrow, leaf, optimize("3"), access(read_only, 1))) // attribute injection for unequality operator
+#define __a_inj_get __attribute__((nothrow, leaf, optimize("0"), pure, always_inline, no_sanitize_address, no_sanitize_undefined, warn_unused_result)) // attribute injection for get method
+#define __a_inj_alloc __attribute__((nothrow, leaf, optimize("3"), access(read_only, 1), always_inline, no_sanitize_address, no_sanitize_undefined)) // attribute injection for alloc method
+#define __a_inj_free __attribute__((nothrow, leaf, optimize("3"), always_inline, stack_protect, no_sanitize_address, no_sanitize_undefined)) // attribute injection for free method
+#define __a_inj_hex_p __attribute__((nothrow, optimize("3"), always_inline, stack_protect, no_sanitize_address, no_sanitize_undefined, warn_unused_result, access(read_only, 1))) // attribute injection for free method
+
+#else
+
+#define __a_inj_dc [[nothrow]]
+#define __a_inj_cctor [[nodiscard, nothrow]]
+#define __a_inj_mctor [[nodiscard, nothrow]]
+#define __a_inj_eqop [[nodiscard, nothrow]]
+#define __a_inj_uneqop [[nodiscard, nothrow]]
+#define __a_inj_get [[nodiscard, nothrow]]
+#define __a_inj_alloc [[nothrow]]
+#define __a_inj_free [[nothrow]]
+#define __a_inj_hex_p [[nodiscard, nothrow]]
 #endif
 
-template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !std::is_pointer_v<_cT>>> class SecureAlloc
+
+template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> &&!std::is_pointer_v<_cT>>> class SecureAlloc
 {
     _cT *_data_;
     size_t _block_size_;
@@ -28,9 +47,9 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
     bool _is_init_ = false;
     std::mutex _Mtx_; // for thread safe access, race conditions, dead-locks, etc...
   public:
-    __attribute__((nothrow, leaf, optimize("3"), section(".text"))) explicit SecureAlloc() noexcept
+    __a_inj_dc explicit SecureAlloc() noexcept
     {
-        static_assert(!std::is_array_v<_cT> && !std::is_pointer_v<_cT>, "no Array allocatable or Pointer...");
+        static_assert(!std::is_array_v<_cT> &&!std::is_pointer_v<_cT>, "no Array allocatable or Pointer...");
         this->_data_ = new (std::nothrow) _cT;
         this->_address_ = this->__HexAddress(reinterpret_cast<std::uintptr_t>(this->_data_));
         this->_block_size_ = sizeof(*this->_data_);
@@ -38,9 +57,9 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
         this->_is_free_ = false;
     };
 
-    __attribute__((nothrow, leaf, optimize("3"), access(read_only, 1))) explicit SecureAlloc(const _cT _Pv) noexcept
+    __a_inj_cctor explicit SecureAlloc(const _cT _Pv) noexcept
     {
-        static_assert(!std::is_array_v<_cT> && !std::is_pointer_v<_cT>, "no Array or Pointer allocatable...");
+        static_assert(!std::is_array_v<_cT> &&!std::is_pointer_v<_cT>, "no Array or Pointer allocatable...");
         this->_data_ = new (std::nothrow) _cT(std::move(_Pv));
         this->_address_ = this->__HexAddress(reinterpret_cast<std::uintptr_t>(this->_data_));
         this->_block_size_ = sizeof(*this->_data_);
@@ -49,13 +68,13 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
     };
 
     // ------ COPY CONSTRUCTOR/+OPERATOR ------
-    __attribute__((nothrow, leaf, optimize("3"), access(read_only, 1))) explicit SecureAlloc(const SecureAlloc &_o) noexcept : _block_size_(_o._block_size_), _Mtx_(_o._Mtx_), _is_free_(_o._is_free_), _is_init_(_o._is_init_)
+    __a_inj_cctor explicit SecureAlloc(const SecureAlloc &_o) noexcept : _block_size_(_o._block_size_), _Mtx_(_o._Mtx_), _is_free_(_o._is_free_), _is_init_(_o._is_init_)
     {
         *this->_data_ = std::move(*_o._data_);
     };
-    __attribute__((nothrow, leaf, optimize("3"), access(read_only, 1))) SecureAlloc &operator=(const SecureAlloc &_o) noexcept
+    __a_inj_eqop SecureAlloc &operator=(const SecureAlloc &_o) noexcept
     {
-        if (*this != _o)
+        if (*this!= _o)
         {
             this->_data_ = std::move(*(_o._data_));
             this->_block_size_ = _o._block_size_;
@@ -67,13 +86,13 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
     };
 
     // ------ MOVE CONSTRUCTOR/+OPERATOR ------
-    __attribute__((nothrow, leaf, optimize("3"), access(read_write, 1))) explicit SecureAlloc(SecureAlloc &&_o) noexcept : _data_(std::move_if_noexcept(_o._data_)), _block_size_(_o._block_size_), _Mtx_(std::move(_o._Mtx_)), _is_free_(_o._is_free_), _is_init_(_o._is_init_)
+    __a_inj_mctor explicit SecureAlloc(SecureAlloc &&_o) noexcept : _data_(std::move_if_noexcept(_o._data_)), _block_size_(_o._block_size_), _Mtx_(std::move(_o._Mtx_)), _is_free_(_o._is_free_), _is_init_(_o._is_init_)
     {
         _o.~SecureAlloc();
     };
-    __attribute__((nothrow, leaf, optimize("3"), access(read_write, 1))) SecureAlloc &operator=(SecureAlloc &&_o) noexcept
+    __a_inj_eqop SecureAlloc &operator=(SecureAlloc &&_o) noexcept
     {
-        if (this != _o)
+        if (this!= _o)
         {
             this->_data_ = std::move_if_noexcept<_cT>(_o._data_);
             this->_block_size_ = _o._block_size_;
@@ -86,30 +105,30 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
     };
 
     // Equality Comparision Operators
-    constexpr inline bool operator==(const SecureAlloc &_o) noexcept
+    __a_inj_eqop constexpr inline bool operator==(const SecureAlloc &_o) noexcept
     {
         return this->_address_ == _o._address_;
     };
 
-    constexpr inline bool operator!=(const SecureAlloc &_o) noexcept
+    __a_inj_uneqop constexpr inline bool operator!=(const SecureAlloc &_o) noexcept
     {
-        return !(this->_address_ == _o._address_);
+        return!(this->_address_ == _o._address_);
     };
 
     /**
      * Get allocated block.
      * @returns _cT*
      */
-    __attribute__((nothrow, leaf, optimize("0"), pure, always_inline, no_sanitize_address, no_sanitize_undefined, warn_unused_result)) constexpr _cT *Get(void) noexcept
+    __a_inj_get constexpr _cT *Get(void) noexcept
     {
-        return this->_is_free_ || !this->_is_init_ ? nullptr : this->_data_;
+        return this->_is_free_ ||!this->_is_init_? nullptr : this->_data_;
     };
 
     /**
      * Get allocated block size.
      * @returns const size_t&
      */
-    __attribute__((nothrow, leaf, optimize("0"), pure, always_inline, no_sanitize_address, no_sanitize_undefined, warn_unused_result)) constexpr size_t &GetSize(void) noexcept
+    __a_inj_get constexpr size_t &GetSize(void) noexcept
     {
         return this->_block_size_;
     };
@@ -118,7 +137,7 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
      * Get Allocation address.
      * @returns const std::uintptr_t&
      */
-    __attribute__((nothrow, leaf, optimize("0"), pure, always_inline, no_sanitize_address, no_sanitize_undefined, warn_unused_result)) constexpr std::uintptr_t &GetAddress(void) noexcept
+    __a_inj_get constexpr std::uintptr_t &GetAddress(void) noexcept
     {
         return this->_address_;
     };
@@ -127,11 +146,11 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
      * Allocate data.
      * @param const _cT data to allocate.
      */
-    __attribute__((nothrow, leaf, optimize("3"), access(read_only, 1), always_inline, no_sanitize_address, no_sanitize_undefined)) constexpr void Alloc(const _cT _mv) noexcept
+    __a_inj_alloc constexpr void Alloc(const _cT _mv) noexcept
     {
         std::lock_guard<std::mutex> _l(this->_Mtx_);
-        static_assert(!std::is_array_v<_cT> && !std::is_pointer_v<_cT>, "no Array or Pointer can be set!");
-        if (this->_is_free_ || !this->_is_init_) [[unlikely]]
+        static_assert(!std::is_array_v<_cT> &&!std::is_pointer_v<_cT>, "no Array or Pointer can be set!");
+        if (this->_is_free_ ||!this->_is_init_) [[unlikely]]
             this->_data_ = new (std::nothrow) _cT(std::move(_mv));
         else
             *this->_data_ = std::move(_mv);
@@ -143,15 +162,15 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
      * check is allocation is null.
      * @returns bool
      */
-    __attribute__((nothrow, leaf, optimize("0"), pure, always_inline, no_sanitize_address, no_sanitize_undefined, warn_unused_result)) constexpr bool IsNullPtr(void) noexcept
+    __a_inj_get constexpr bool IsNullPtr(void) noexcept
     {
-        return this->_is_free_ || !this->_is_init_ || this->_data_ == nullptr;
+        return this->_is_free_ ||!this->_is_init_ || this->_data_ == nullptr;
     };
 
     /**
      * Free allocated data
      */
-    __attribute__((nothrow, leaf, optimize("3"), always_inline, stack_protect, no_sanitize_address, no_sanitize_undefined)) inline void Free(void) noexcept
+    __a_inj_free inline void Free(void) noexcept
     {
         std::lock_guard<std::mutex> _l(this->_Mtx_);
         if (!this->_is_free_ && this->_is_init_) [[likely]]
@@ -174,7 +193,7 @@ template <typename _cT, typename = std::enable_if<!std::is_reference_v<_cT> && !
      * @param const std::uintptr_t&
      * @returns const std::uintptr_t
      */
-    const std::uintptr_t __HexAddress(const std::uintptr_t &_addr) noexcept
+    __a_inj_hex_p const std::uintptr_t __HexAddress(const std::uintptr_t &_addr) noexcept
     {
         size_t hex_value;
         std::stringstream _p;
